30fca20df4246dde1a3e2d3c9408f3e1ab40e7c5
grushler <girishsortur@gmail.com>
2021-11-08 19:53:20 +0530

    Initial commit with code for get and post api

---
+++ app.py
@@ -0,0 +1,22 @@
+import json
+from flask import Flask, jsonify, request
+
+from .services import retrieve_orders, create_order
+
+app = Flask(__name__)
+
+@app.route('/health')
+def health():
+    return jsonify({'response': 'Hello World!'})
+
+@app.route('/api/orders/computers', methods=['GET', 'POST'])
+def computer_orders():
+    if request.method == 'GET':
+        return jsonify(retrieve_orders())
+    elif request.method == 'POST':
+        return jsonify(create_order(request.json))
+    else:
+        raise Exception('Unsupported HTTP request type.')
+
+if __name__ == '__main__':
+    app.run()
---
+++ enums.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class Status(Enum):
+    Queued = 'Queued'
+    Processing = 'Processing'
+    Completed = 'Completed'
+    Failed = 'Failed'
+    Cancelled = 'Cancelled'
